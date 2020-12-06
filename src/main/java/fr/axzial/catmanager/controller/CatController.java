10da249424c4eb1af2bebdf685567b6e7a087878
Victor Brown <victorbrown37@protonmail.com>
2020-12-06 16:01:48 +0100

    :rocket: Implementing Cat Controller with Service Interface

--- src/main/java/fr/axzial/catmanager/controller/CatController.java
+++ src/main/java/fr/axzial/catmanager/controller/CatController.java
@@ -1,9 +1,54 @@
 package fr.axzial.catmanager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.axzial.catmanager.dto.CatBreedDto;
+import fr.axzial.catmanager.dto.CatDto;
+import fr.axzial.catmanager.exception.BreedNotFoundException;
+import fr.axzial.catmanager.exception.CatNotFoundException;
+import fr.axzial.catmanager.model.Cat;
+import fr.axzial.catmanager.model.CatBreed;
+import fr.axzial.catmanager.service.CatService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cat")
 public class CatController {
+
+    private final CatService catService;
+
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cat>> getCats(){
+        List<Cat> catList = catService.findAll();
+        return new ResponseEntity<>(catList, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cat> getCat(@PathVariable long id){
+        return catService.findById(id).map(ResponseEntity::ok)
+                .orElseThrow(() -> new CatNotFoundException("Can't find Cat with id: " + id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Cat> addCat(@RequestBody CatDto catDto){
+        return new ResponseEntity<>(catService.save(catDto), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Cat> update(@PathVariable long id, @RequestBody CatDto catDto){
+        return catService.update(id, catDto).map(ResponseEntity::ok)
+                .orElseThrow(() -> new CatNotFoundException("Can't find Cat with id: " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        catService.delete(id);
+    }
 }
--- src/main/java/fr/axzial/catmanager/service/CatService.java
+++ src/main/java/fr/axzial/catmanager/service/CatService.java
@@ -1,4 +1,16 @@
 package fr.axzial.catmanager.service;
 
+import fr.axzial.catmanager.dto.CatDto;
+import fr.axzial.catmanager.exception.CatNotFoundException;
+import fr.axzial.catmanager.model.Cat;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CatService {
+    List<Cat> findAll();
+    Optional<Cat> findById(long id);
+    Cat save(CatDto catDto);
+    Optional<Cat> update(long id, CatDto catDto) throws CatNotFoundException;
+    void delete(long id) throws CatNotFoundException;
 }
