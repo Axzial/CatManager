499f7a77678b784888edf5dfeb48f00fde20beb0
Victor Brown <victorbrown37@protonmail.com>
2020-12-06 15:16:39 +0100

    :rocket: Implementing Controllers

---
+++ src/main/java/fr/axzial/catmanager/controller/CatController.java
@@ -0,0 +1,9 @@
+package fr.axzial.catmanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cat")
+public class CatController {
+}
