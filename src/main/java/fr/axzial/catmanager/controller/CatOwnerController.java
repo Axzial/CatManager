b794f35a13c7174cf03e7836078ffeb7b5daf25a
Victor Brown <victorbrown37@protonmail.com>
2020-12-06 16:14:13 +0100

    :rocket: Implementing CatOwner Controller with Service Interface

--- src/main/java/fr/axzial/catmanager/controller/CatOwnerController.java
+++ src/main/java/fr/axzial/catmanager/controller/CatOwnerController.java
@@ -1,9 +1,50 @@
 package fr.axzial.catmanager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.axzial.catmanager.dto.CatOwnerDto;
+import fr.axzial.catmanager.exception.CatOwnerNotFoundException;
+import fr.axzial.catmanager.model.CatOwner;
+import fr.axzial.catmanager.service.CatOwnerService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/catowner")
 public class CatOwnerController {
+
+    private final CatOwnerService catOwnerService;
+
+    public CatOwnerController(CatOwnerService catOwnerService) {
+        this.catOwnerService = catOwnerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CatOwner>> getOwners(){
+        return new ResponseEntity<>(catOwnerService.findAll(), new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CatOwner> getOwner(@PathVariable long id){
+        return catOwnerService.findById(id).map(ResponseEntity::ok)
+                .orElseThrow(() -> new CatOwnerNotFoundException("Can't find CatOwner with id: " + id));
+    }
+
+    @PutMapping
+    public ResponseEntity<CatOwner> addCat(@RequestBody CatOwnerDto catOwnerDto){
+        return new ResponseEntity<>(catOwnerService.save(catOwnerDto), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CatOwner> update(@PathVariable long id, @RequestBody CatOwnerDto catOwnerDto){
+        return catOwnerService.update(id, catOwnerDto).map(ResponseEntity::ok)
+                .orElseThrow(() -> new CatOwnerNotFoundException("Can't find CatOwner with id: " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        catOwnerService.delete(id);
+    }
 }
--- src/main/java/fr/axzial/catmanager/service/CatOwnerService.java
+++ src/main/java/fr/axzial/catmanager/service/CatOwnerService.java
@@ -1,4 +1,18 @@
 package fr.axzial.catmanager.service;
 
+import fr.axzial.catmanager.dto.CatBreedDto;
+import fr.axzial.catmanager.dto.CatOwnerDto;
+import fr.axzial.catmanager.exception.BreedNotFoundException;
+import fr.axzial.catmanager.model.CatBreed;
+import fr.axzial.catmanager.model.CatOwner;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CatOwnerService {
+    List<CatOwner> findAll();
+    Optional<CatOwner> findById(long id);
+    CatOwner save(CatOwnerDto catOwnerDto);
+    Optional<CatOwner> update(long id, CatOwnerDto catOwnerDto) throws BreedNotFoundException;
+    void delete(long id) throws BreedNotFoundException;
 }
