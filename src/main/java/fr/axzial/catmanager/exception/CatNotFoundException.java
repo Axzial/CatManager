4ff77b00636ba55898bfadfe3f3efd97afb2c0f5
Victor Brown <victorbrown37@protonmail.com>
2020-12-08 01:52:23 +0100

    :sparkles: Give exceptions status codes

--- src/main/java/fr/axzial/catmanager/exception/CatNotFoundException.java
+++ src/main/java/fr/axzial/catmanager/exception/CatNotFoundException.java
@@ -1,8 +1,12 @@
 package fr.axzial.catmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * The {@link fr.axzial.catmanager.model.Cat} not found exception.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CatNotFoundException extends RuntimeException {
 
     public CatNotFoundException() {
--- src/main/java/fr/axzial/catmanager/exception/CatOwnerNotFoundException.java
+++ src/main/java/fr/axzial/catmanager/exception/CatOwnerNotFoundException.java
@@ -1,8 +1,12 @@
 package fr.axzial.catmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * The {@link fr.axzial.catmanager.model.CatOwner} not found exception.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CatOwnerNotFoundException extends RuntimeException {
 
     public CatOwnerNotFoundException() {
